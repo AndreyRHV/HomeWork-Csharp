d90af5eeb1798d11feaa35018021b8f0e7e9c28f
Andrey <arpbww@rambler.ru>
2022-09-02 19:07:29 +0700

    Home work 1 seminar

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Task2
+
+// Console.Write("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b){
+//     Console.Write("max = ");
+//     Console.WriteLine(a);
+//     Console.Write("min = ");
+//     Console.Write(b);
+    
+// }
+// else{
+//     Console.Write("max = ");
+//     Console.WriteLine(b);
+//     Console.Write("min = ");
+//     Console.Write(a);
+// }
+
+
+// Task4
+
+// Console.Write("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число c: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+//  if (b > max){
+//     max = b;
+//  }
+//  if (c > max){
+//     max = c;
+//  }
+
+// System.Console.WriteLine(max);
+
+
+// Task6
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a % 2 == 0){
+//     System.Console.WriteLine("Да");
+// }
+// else{
+//     System.Console.WriteLine("Нет");
+// }
+
+
+// Task8
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int a = 1;
+
+// while (a <= n){
+//     if (a % 2 == 0){
+//     Console.Write(a+" ");
+//     a++;
+//     }
+//     else{
+//         a++;
+//     }
+// }
